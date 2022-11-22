34f4b090d4895b1fb1f8d89de7ff4a8dc2c5c494
12den24 <12den24@gmail.com>
2022-11-22 13:32:27 +0300

    add

---
+++ Task_2/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Task_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 26);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(185, 394);
+            this.treeView1.TabIndex = 0;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(213, 26);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(563, 394);
+            this.listBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ Task_2/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+/*Написать программу «Проводник»
+ ■ При первом запуске программа отображает список доступ-
+ных дисков;
+ ■ Программа должна содержать дерево дисков, строку адре-
+са, меню, панель инструментов и окно для отображения со-
+держимого папки;
+ ■ Дерево дисков отображает только диски и папки (можно 
+реализовать с помощью ListBox);
+■ При двойном щелчке по папке – окно содержимого отобра-
+жаются файлы и подпапки;
+ ■ Программа должна иметь развернутое меню, контекстное 
+меню и возможность работы с горячими клавишами.*/
+
+namespace Task_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            treeView1.BeforeSelect += treeView1_BeforeSelect;
+            treeView1.BeforeExpand += treeView1_BeforeExpand;
+            // заполняем дерево дисками
+            FillDriveNodes();
+        }
+
+        // событие перед раскрытием узла
+        void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    Text = e.Node.FullPath;
+                    dirs = Directory.GetDirectories(e.Node.FullPath);
+                    if (dirs.Length != 0)
+                    {
+                        for (int i = 0; i < dirs.Length; i++)
+                        {
+                            TreeNode dirNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                            FillTreeNode(dirNode, dirs[i]);
+                            e.Node.Nodes.Add(dirNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        // событие перед выделением узла
+        void treeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    Text = e.Node.FullPath;
+                    dirs = Directory.GetDirectories(e.Node.FullPath);
+                    if (dirs.Length != 0)
+                    {
+                        for (int i = 0; i < dirs.Length; i++)
+                        {
+                            TreeNode dirNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                            FillTreeNode(dirNode, dirs[i]);
+                            e.Node.Nodes.Add(dirNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        // получаем все диски на компьютере
+        private void FillDriveNodes()
+        {
+            try
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    TreeNode driveNode = new TreeNode { Text = drive.Name };
+                    FillTreeNode(driveNode, drive.Name);
+                    treeView1.Nodes.Add(driveNode);
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        // получаем дочерние узлы для определенного узла
+        private void FillTreeNode(TreeNode driveNode, string path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    TreeNode dirNode = new TreeNode();
+                    dirNode.Text = dir.Remove(0, dir.LastIndexOf("\\") + 1);
+                    driveNode.Nodes.Add(dirNode);
+                }
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
